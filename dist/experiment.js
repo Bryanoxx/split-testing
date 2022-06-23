20b09e8166b93f2c285235f89c2c3591130ee551
Bryanoxx <contact@bryanparasote.fr>
2022-06-23 11:39:33 +0100

    chore: bundle files

--- dist/experiment.js
+++ dist/experiment.js
@@ -12,29 +12,30 @@ const utils_1 = require("./utils/utils");
  */
 function setExperiment(options) {
     var _a, _b, _c;
+    // Validation of the options
+    if (typeof options.name !== 'string' || options.name.length === 0) {
+        throw (0, utils_1.createError)('The experiment name must be a non-empty string');
+    }
+    if (!Array.isArray(options.variants) || options.variants.length === 0) {
+        throw (0, utils_1.createError)('The variants must be an array of at least one element');
+    }
+    const variantsHaveNames = options.variants.every(variant => variant.name !== undefined && variant.name.length > 0);
+    if (!variantsHaveNames) {
+        throw (0, utils_1.createError)('The variants must have a name');
+    }
+    options.storage = (_a = options.storage) !== null && _a !== void 0 ? _a : (globalThis.window !== undefined ? globalThis.window.localStorage : undefined);
+    if (options.storage === undefined) {
+        throw (0, utils_1.createError)('No storage available, please define a custom storage property');
+    }
     // Extraction of the options
     const experiment = {
         name: options.name,
         variants: (0, utils_1.deepClone)(options.variants),
         seed: options.seed,
-        isDebugMode: (_a = options.isDebugMode) !== null && _a !== void 0 ? _a : false,
-        isResolvingSeedConflictAllowed: (_b = options.isResolvingSeedConflictAllowed) !== null && _b !== void 0 ? _b : true,
-        storage: (_c = options.storage) !== null && _c !== void 0 ? _c : window === null || window === void 0 ? void 0 : window.localStorage
+        isDebugMode: (_b = options.isDebugMode) !== null && _b !== void 0 ? _b : false,
+        isResolvingSeedConflictAllowed: (_c = options.isResolvingSeedConflictAllowed) !== null && _c !== void 0 ? _c : true,
+        storage: options.storage
     };
-    // Validation of the options
-    if (typeof experiment.name !== 'string' || experiment.name.length === 0) {
-        throw (0, utils_1.createError)('The experiment name must be a non-empty string');
-    }
-    if (!Array.isArray(experiment.variants) || experiment.variants.length === 0) {
-        throw (0, utils_1.createError)('The variants must be an array of at least one element');
-    }
-    const variantsHaveNames = experiment.variants.every(variant => variant.name !== undefined && variant.name.length > 0);
-    if (!variantsHaveNames) {
-        throw (0, utils_1.createError)('The variants must have a name');
-    }
-    if ((window === null || window === void 0 ? void 0 : window.localStorage) == null && options.storage == null) {
-        throw (0, utils_1.createError)('No storage available, please define a custom storage property');
-    }
     // Configuration of the logget depending on the debug mode
     const logger = (0, utils_1.makeLogger)(experiment.isDebugMode);
     logger('Running split testing with these options :');
